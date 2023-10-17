487610d9529bbdfbcaa792522c442578f914ff94
afif1731 <afifsdi@gmail.com>
2023-10-18 00:27:58 +0700

    update on client_vm

--- client_vm/erpc_client.py
+++ client_vm/erpc_client.py
@@ -6,28 +6,54 @@ import os
 proxy = ServerProxy('http://10.0.2.2:3000')
 
 thisdir = os.path.dirname(os.path.realpath(__file__))
-mode = sys.argv[1]
-filename = sys.argv[2]
 
-if mode == "send":
+def mode_send(filename):
 	filepath = thisdir + '/' + filename
 	if not os.path.exists(filepath):
 		print('There is no such a thing called {}'.format(filename))
-		sys.exit(1)
+		return 0
 	
 	with open(filepath, "rb") as handle:
 		data_in_binary = Binary(handle.read())
 		proxy.file_to_server(data_in_binary, filename)
 		handle.close()
+	print('{} sended to the server'.format(filename))
 	
-elif mode == "get":
+def mode_get(filename):
 	data_binary = proxy.client_get_file(filename)
 	filepath = thisdir + '/' + filename
 	
 	if data_binary == False:
 		print('There is no such a thing called {} in the server'.format(filename))
-		sys.exit(1)
+		return 0
 		
 	with open(filepath, "wb") as handle:
 		handle.write(data_binary.data)
 		handle.close()
+	print('{} received'.format(filename))
+
+print('Client is now activated')
+while True:
+	arg = input('> ')
+	argv = arg.split()
+
+	mode = argv[0]
+	filename = None
+	if len(argv) == 2:
+		filename = argv[1]
+	
+	if mode == 'quit':
+		print('Closing Client...')
+		sys.exit(1)
+	elif mode == 'get':
+		if filename is not None:
+			mode_get(filename)
+		else :
+			print('Missing argumen: use "get [filename]"')
+	elif mode == 'send':
+		if filename is not None:
+			mode_send(filename)
+		else :
+			print('Missing argumen: use "send [filename]"')
+	else :
+		print('Invalid Mode!')
